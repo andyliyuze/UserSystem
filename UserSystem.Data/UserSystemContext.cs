4f65c34521dc702279db2035849b558422a19d6f
andy <andy@DESKTOP-OBCLGH4>
2017-12-24 23:04:12 +0800

    添加项目文件。

---
+++ UserSystem.Data/UserSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserSystem.Core.Entity;
+
+namespace UserSystem.Data
+{
+    public class UserSystemContext: IdentityDbContext<User>
+    {
+        public UserSystemContext()
+            : base("UserSystem")
+        {
+            //调试开发使用，每次都会先删除数据库再创建
+            //   Database.SetInitializer<UserSystemContext>(new DropCreateDatabaseAlways<UserSystemContext>());
+
+        }
+    }
+}
