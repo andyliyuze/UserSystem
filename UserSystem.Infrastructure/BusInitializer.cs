17661b613b66386afff4d6ea29cb7dfc963ee741
andyliyuze <874787504@qq.com>
2017-12-29 17:04:54 +0800

    7444

--- UserSystem.Infrastructure/BusInitializer.cs
+++ UserSystem.Infrastructure/BusInitializer.cs
@@ -8,33 +8,17 @@ namespace UserSystem.Infrastructure
 {
     public class BusInitializer
     {
-        //public static IServiceBus CreateBus(string queueName, Action<ServiceBusConfigurator> moreInitialization)
-        //{
-        //    Log4NetLogger.Use();
-
-        //    var bus = Bus.Factory.CreateUsingRabbitMq ServiceBusFactory.New(x =>
-        //    {
-        //        x.UseRabbitMq();
-        //        x.ReceiveFrom("rabbitmq://localhost/Loosely_" + queueName);
-        //        moreInitialization(x);
-        //    });
-
-        //    return bus;
-        //}
-
-
         public static IBusControl CreateBus()
         {
 
             return Bus.Factory.CreateUsingRabbitMq(sbc =>
             {
-                var host = sbc.Host("localhost","userSystem", h =>
-                {
-                    h.Username("guest");
-                    h.Password("guest");
-                });
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                 {
+                     h.Username("guest");
+                     h.Password("guest");
+                 });
             });
         }
-
     }
 }
